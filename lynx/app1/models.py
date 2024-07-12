9fd35f634ebd53243bb90f5ed5269e271ca40b9b
mathewalex806 <mathewalex806@gmail.com>
2024-07-12 10:35:08 +0530

    Adding Community model

--- lynx/app1/models.py
+++ lynx/app1/models.py
@@ -16,3 +16,22 @@ class User(AbstractUser):
         help_text='Specific permissions for this user.',
         verbose_name='user permissions'
     )
+
+    def __str__(self):
+        return self.username
+
+
+#Adding Community table
+
+class Community (models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(blank=True, null=True)
+    city = models.CharField(max_length=50,blank=False, null=False)
+    locality = models.CharField(max_length=50)
+    country = models.CharField(max_length=70, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
